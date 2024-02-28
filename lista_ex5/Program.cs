23de486da1fd48d25271c130786150a0d5c15946
ALUNO <ALUNO@LABFPC22>
2024-02-28 12:30:02 -0300

    Adicionar arquivos de projeto.

---
+++ lista_ex5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double a, b, m;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            m = (a * 3.5 + b * 7.5) / 11;
+
+            
+            Console.WriteLine("MEDIA = " + String.Format("{0:F5}",m ));
+            Console.ReadKey();
+
+        }
+    }
+}
